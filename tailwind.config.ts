48b0354cfa1aaddab80d551f67eb7496a9c4ef07
dovydasvg <dovydasvg@yahoo.fr>
2024-01-17 18:04:27 +0200

    chore: install daisyui;

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,5 +10,17 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#600FFF",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+        },
+      },
+    ],
+  },
 } satisfies Config;
