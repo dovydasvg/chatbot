6d27c057bb019fd0083067766070859be373582b
dovydasvg <dovydasvg@yahoo.fr>
2024-01-17 18:59:13 +0200

    feat: add chat loader

---
+++ src/components/chat/ChatLoader.tsx
@@ -0,0 +1,9 @@
+export default function ChatLoader() {
+  return (
+    <div className="chat chat-start">
+      <div className="chat-bubble-primary chat-bubble rounded">
+        <span className="loading loading-dots loading-sm"></span>
+      </div>
+    </div>
+  );
+}
