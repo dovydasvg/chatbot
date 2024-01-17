ea3f127e09e7d2150ede7493d53c5a2164687641
dovydasvg <dovydasvg@yahoo.fr>
2024-01-17 18:10:02 +0200

    feat: make avatar smaller;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="flex w-full items-center gap-6 border-b px-6 py-4">
       <div className="avatar">
-        <div className="w-16 rounded-full">
+        <div className="w-14 rounded-full">
           <img src="https://res-2.cloudinary.com/dcp9dwff4/image/upload/t_webp/8eo31x90fecmjjbhaloa7gh05k0x" />
         </div>
       </div>
