af9dcaea239e16f202ffe932be4355e6154583c1
dovydasvg <dovydasvg@yahoo.fr>
2024-01-17 18:09:52 +0200

    feat: add max height and overflow;

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ChatBox from "./ChatBox";
+import { api } from "~/utils/api";
 
 type Chat = {
   message: string;
@@ -8,7 +9,83 @@ type Chat = {
 };
 
 export default function Chat() {
+  const hello = api.post.hello.useQuery({ text: "from tRPC" });
   const chats: Chat[] = [
+    {
+      message: "Hello there!",
+      time: "2 hours ago",
+      sender: "bot",
+    },
+    {
+      message: "General Kenobi!",
+      time: "2 hours ago",
+      sender: "user",
+    },
+    {
+      message: "You are a bold one.",
+      time: "2 hours ago",
+      sender: "bot",
+    },
+    {
+      message: "Hello there!",
+      time: "2 hours ago",
+      sender: "bot",
+    },
+    {
+      message: "General Kenobi!",
+      time: "2 hours ago",
+      sender: "user",
+    },
+    {
+      message: "You are a bold one.",
+      time: "2 hours ago",
+      sender: "bot",
+    },
+    {
+      message: "Hello there!",
+      time: "2 hours ago",
+      sender: "bot",
+    },
+    {
+      message: "General Kenobi!",
+      time: "2 hours ago",
+      sender: "user",
+    },
+    {
+      message: "You are a bold one.",
+      time: "2 hours ago",
+      sender: "bot",
+    },
+    {
+      message: "Hello there!",
+      time: "2 hours ago",
+      sender: "bot",
+    },
+    {
+      message: "General Kenobi!",
+      time: "2 hours ago",
+      sender: "user",
+    },
+    {
+      message: "You are a bold one.",
+      time: "2 hours ago",
+      sender: "bot",
+    },
+    {
+      message: "Hello there!",
+      time: "2 hours ago",
+      sender: "bot",
+    },
+    {
+      message: "General Kenobi!",
+      time: "2 hours ago",
+      sender: "user",
+    },
+    {
+      message: "You are a bold one.",
+      time: "2 hours ago",
+      sender: "bot",
+    },
     {
       message: "Hello there!",
       time: "2 hours ago",
@@ -26,7 +103,7 @@ export default function Chat() {
     },
   ];
   return (
-    <div className="px-6 py-4">
+    <div className="overflow-auto px-6 py-4">
       {chats.map((chat) => (
         <ChatBox
           message={chat.message}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,18 @@
 import Head from "next/head";
-import Link from "next/link";
+
 import Footer from "~/components/Footer";
 import Header from "~/components/Header";
 import Chat from "~/components/chat/Chat";
 
-import { api } from "~/utils/api";
-
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
         <title>The Great ChatBot</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-800 font-mono text-gray-50">
-        <div className="container flex w-full max-w-lg flex-col rounded-lg border border-gray-50">
+      <main className="flex  min-h-screen flex-col items-center justify-center bg-gray-800  font-mono text-gray-50">
+        <div className="container flex max-h-[99dvh] w-full max-w-lg flex-col rounded-lg border border-gray-50">
           <Header />
           <Chat />
           <Footer />
